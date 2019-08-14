315eab87dc4b6ae84e307b5153e73d5622045c4b
DeepaliKalagate <dipakalagate1991@gmail.com>
2019-08-14 04:56:28 -0400

    my programs

--- src/com/bridgelabz/functional/Pattern.java
+++ src/com/bridgelabz/functional/Pattern.java
@@ -1,14 +1,34 @@
 package com.bridgelabz.functional;
 
+import java.util.Scanner;
+
+import com.bridgelabz.utility.Utility;
+
 public class Pattern {
 
 	public static void main(String[] args) 
 	{
-		int i,j,k,count;
-		for(i=0;i<=11;i++)
+		Utility utility=new Utility();
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter string :  ");
+		String input=scanner.next();
+		input=input.toLowerCase();
+		char alphabet=input.charAt(0);
+		switch(alphabet)
 		{
+		case 'k': 
+				utility.printKPattern();
+		
+		case 'd': 
+				utility.printDPattern();
+		
+		case 'w': 
+				utility.printWPattern();
+				break;
+		default:
+			System.out.println("Enter Valid String !");
 		}
-
+		scanner.close();
 	}
 
 }
