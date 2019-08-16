3eae291599c6f76dafb94216cfa3764e0bd458d1
DeepaliKalagate <53900826+DeepaliKalagate@users.noreply.github.com>
2019-08-16 07:44:08 -0400

    Update SquareRoot.java

--- src/com/bridgelabz/functional/SquareRoot.java
+++ src/com/bridgelabz/functional/SquareRoot.java
@@ -6,26 +6,25 @@ public class SquareRoot
 {        
 	public static void main(String[] args) 
 	{
-		int n;
 		Scanner scanner=new Scanner(System.in);
-		System.out.println("Enter number : ");
-		n=scanner.nextInt();
+		System.out.println("Enter number: ");
+		int n=scanner.nextInt();
 		int a;
 		for(int i=0;i<n;i++)
 		{
-			if(n!=0)
+			 if(n==1)
+			 {
+				System.out.println("Check Number");
+					
+			 }
+			else 
 			{
 				n=n%10;
 				n=n/10;
 				a=n*n;
-				System.out.println("RESULT "+a);
+				System.out.println("RESULT is: "+a);
 			}
-			else
-			{
-				System.out.println("Check Number");
-				
-			}
-
+			
 		}
 	}
 }
